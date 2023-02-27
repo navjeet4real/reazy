61555cb56b2b207d796aff8afc3ac79915973295
Rabin Mishra <rabin.immanent@gmail.com>
2023-02-27 10:56:18 +0530

    cors

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,8 @@
 import { PATH_DASHBOARD } from "./routes/paths";
 
-export const BASE_URL = "http://localhost:8000"
+export const BASE_URL = "http://3.105.195.195:8000"
+// export const BASE_URL = "http://localhost:8000"
+
 
 
 export const DEFAULT_PATH = PATH_DASHBOARD.general.app; // as '/app'
